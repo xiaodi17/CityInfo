b30b0b5e3f196150198bab5feefe53e106cf06a7
Ding Lu <n8477621@qut.edu.au>
2020-03-03 22:46:50 +1000

    CityDto and PointsOfInterestDto

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCitites()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            //find city
+            var cityModel = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            
+            if(cityModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityModel);
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPointOfInterest(int cityId, int id)
+        {
+            var cityModel = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (cityModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityModel.PointsOfInterest);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPointOfInterestById(int cityId, int id)
+        {
+            var cityModel = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (cityModel == null)
+            {
+                return NotFound();
+            }
+
+            //find point of interest
+            var pointOfInterest = cityModel.PointsOfInterest.FirstOrDefault(c => c.Id == id);
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+            return Ok(pointOfInterest);
+        }
+    }
+}
